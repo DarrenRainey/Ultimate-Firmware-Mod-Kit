5c2e68ee38c4cfc98277c7f6a856207564396a72
Darren Rainey <darren@darrenraineys.co.uk>
2017-05-02 18:19:25 +0100

    added squashfs-tools

--- grab-tools-debain.sh
+++ grab-tools-debain.sh
@@ -1,4 +1,4 @@
-apt install fastjar git python python-dev python-lzma
+apt install fastjar git python python-dev python-lzma squashfs-tools
 git clone https://github.com/devttys0/binwalk
 cd binwalk
 python setup.py install
